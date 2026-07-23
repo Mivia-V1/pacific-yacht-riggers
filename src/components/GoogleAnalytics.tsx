37d0aef42411b7f82c50602cdfe0bf087cf6c069
Gavin <gavin.werner@me.com>
2026-07-23 19:15:28 +1200

    Initialize analytics before tag load

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,30 +1,33 @@
 "use client";
 
 import { useEffect } from "react";
+import { GA4_ID } from "@/lib/constants";
 
 declare global {
   interface Window {
     dataLayer: unknown[];
+    gtag?: (...args: unknown[]) => void;
   }
 }
 
-const GA4_ID = "G-C78T6B4P4R";
-
 export default function GoogleAnalytics() {
   useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = (...args: unknown[]) => {
+      window.dataLayer.push(args);
+    };
+    window.gtag("js", new Date());
+    window.gtag("config", GA4_ID);
+
+    if (document.getElementById("google-analytics")) {
+      return;
+    }
+
     const script = document.createElement("script");
+    script.id = "google-analytics";
     script.src = `https://www.googletagmanager.com/gtag/js?id=${GA4_ID}`;
     script.async = true;
     document.head.appendChild(script);
-
-    script.onload = () => {
-      window.dataLayer = window.dataLayer || [];
-      function gtag(...args: unknown[]) {
-        window.dataLayer.push(args);
-      }
-      gtag("js", new Date());
-      gtag("config", GA4_ID);
-    };
   }, []);
 
   return null;
